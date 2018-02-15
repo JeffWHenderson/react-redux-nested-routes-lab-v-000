4b62b711c0bd491943c8b7bfa4a08f688872e628
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-15 21:33:34 +0000

    automatically backed up by learn

--- src/containers/PetsShow.js
+++ src/containers/PetsShow.js
@@ -9,6 +9,11 @@ const PetsShow = ({ pet }) =>
 
 const mapStateToProps = (state, ownProps) => {
   const pet = state.pets.find(pet => pet.id === ownProps.match.params.petID)
+  if (pet) {
+    return  { pet }
+  } else {
+    return { pet: {} }
+  }
 };
 
 export default connect(mapStateToProps)(PetsShow);
